0d69a1c89a6f2b785a6950ee4ad912a1862cb198
tkmct <1220t.takamichi@gmail.com>
2020-01-06 22:09:27 +0900

    add startup modal

---
+++ src/components/StartupModal.js
@@ -0,0 +1,19 @@
+const StartUpModal = () => {
+  return (
+    <div className="container">
+      <p>startup modal</p>
+      <style jsx>{`
+        .container {
+          display: fixed;
+          width: 100vw;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background-color: white;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default StartUpModal
