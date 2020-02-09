02abde6b53cf94d1a55659a806a458e165912d64
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2020-02-09 19:14:46 +0900

    added modal background reference in colors.js

--- src/colors.js
+++ src/colors.js
@@ -2,5 +2,6 @@ export const BACKGROUND = '#1B191A'
 export const TEXT = '#fff'
 export const SUBTEXT = 'rgba(255,255,255,0.5)'
 export const SECTION_BACKGROUND = 'rgba(255, 255, 255, 0.08)'
+export const MODAL_BACKGROUND = 'rgba(0, 0, 0, 0.8)'
 
 export const AREA = '#2D2A2C'
