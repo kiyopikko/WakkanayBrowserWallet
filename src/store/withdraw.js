ef5a65369e79fa210395d0cd749b5264349f861e
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2020-02-20 00:55:15 +0900

    add withdraw thunk

--- src/store/withdraw.js
+++ src/store/withdraw.js
@@ -1,4 +1,5 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
+import clientWrapper from '../client'
 
 export const setWithdrawnToken = createAction('SET_WITHDRAWN_TOKEN')
 export const setWithdrawPage = createAction('SET_WITHDRAW_PAGE')
@@ -17,3 +18,16 @@ export const withdrawReducer = createReducer(
     }
   }
 )
+
+export const withdraw = amount => {
+  return async dispatch => {
+    try {
+      const client = await clientWrapper.getClient()
+      if (!client) return
+      await client.withdraw(amount)
+      dispatch(setWithdrawPage('completion-page'))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
