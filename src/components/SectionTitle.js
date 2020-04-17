8c5019c8eb762b8092a7e3b5907466ba9eb914c7
Kiyohiko Heima <piyo326@gmail.com>
2020-04-18 01:39:46 +0900

    Change section title style

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,12 +1,15 @@
-import { BOLD, LARGE } from '../fonts'
+import { FW_BLACK, FZ_MEDIUM } from '../fonts'
+import { SUBTEXT } from '../colors'
 
 export const SectionTitle = props => {
   return (
     <div className="section-title">
       {props.children}
       <style jsx>{`
-        font-weight: ${BOLD};
-        font-size: ${LARGE};
+        font-weight: ${FW_BLACK};
+        font-size: ${FZ_MEDIUM};
+        color: ${SUBTEXT};
+        margin-bottom: 0.625rem;
       `}</style>
     </div>
   )
