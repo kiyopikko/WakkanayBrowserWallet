b4e08241a8461aeedea39ae1d38550ef5c92a1f0
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-05-07 21:51:10 +0900

    TMP

--- src/store/transaction_history.js
+++ src/store/transaction_history.js
@@ -1,10 +1,32 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
+import clientWrapper from '../client'
 
-export const setFilter = createAction('SET_FILTER')
+export const setHistoryList = createAction('SET_HISTORY_LIST')
 
-export const filterReducer = createReducer('Filter', {
-  [setFilter]: (state, action) => action.payload
-})
+export const filterReducer = createReducer(
+  {
+    historyList: []
+  },
+  {
+    [setHistoryList]: (state, action) => {
+      state.historyList = action.payload
+    }
+  }
+)
+
+export const getTransactionHistories = () => {
+  return async dispatch => {
+    try {
+      const client = await clientWrapper.getClient()
+      if (!client) return
+      const histories = await client.getAllUserActions()
+      console.log('HISTORIES', histories)
+      dispatch(setHistoryList(histories))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 
 // state = {
 // currentFilter: "Filter ▽" | "Address" | "Tokens" | "ENS" | "Block #" | "Range"
