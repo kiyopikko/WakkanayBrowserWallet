7236857861f885a1bf782d7a9391cfcb7ef6537f
Shuhei Hiya <syuhei176@gmail.com>
2020-07-14 19:02:50 +0900

    add comment

--- src/wallet/MetamaskService.js
+++ src/wallet/MetamaskService.js
@@ -9,12 +9,14 @@ export class MetamaskService {
     if (typeof window.ethereum === undefined) {
       throw new Error('cannot find ethereum object.')
     }
+    // to support privacy mode of MetaMask
     await window.ethereum.enable()
     window.web3 = new Web3(window.ethereum)
     const provider = new ethers.providers.Web3Provider(
       window.web3.currentProvider
     )
     const address = await provider.getSigner().getAddress()
+    // wait the network is ready
     const network = await provider.ready
     if (networkName !== 'local' && network.name !== networkName) {
       throw new Error(
