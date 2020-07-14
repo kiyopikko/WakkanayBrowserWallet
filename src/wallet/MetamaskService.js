7d04471e33853666355f7448eb40173bdd42a236
Shuhei Hiya <syuhei176@gmail.com>
2020-07-14 19:00:57 +0900

    support privacy mode
    enable ethereum object to support MetaMask's privacy mode.
    see https://github.com/ethers-io/ethers.js/issues/433

--- src/wallet/MetamaskService.js
+++ src/wallet/MetamaskService.js
@@ -6,7 +6,14 @@ import { ethers } from 'ethers'
  */
 export class MetamaskService {
   static async initialize(networkName) {
-    const provider = new ethers.providers.Web3Provider(web3.currentProvider)
+    if (typeof window.ethereum === undefined) {
+      throw new Error('cannot find ethereum object.')
+    }
+    await window.ethereum.enable()
+    window.web3 = new Web3(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(
+      window.web3.currentProvider
+    )
     const address = await provider.getSigner().getAddress()
     const network = await provider.ready
     if (networkName !== 'local' && network.name !== networkName) {
